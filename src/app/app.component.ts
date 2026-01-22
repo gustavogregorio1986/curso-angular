3b425ce455201c63d5d8a3e97b309846ffdb8fd6
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-22 11:53:21 -0300

    Desnevolver as directivas no projeto formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PrimeiroComponenteComponent } from "./primeiro-componente/primeiro-componente.component";
 import { DataBindingsComponent } from "./data-bindings/data-bindings.component";
+import { FormularioComponent } from "./formulario/formulario.component";
 
 @Component({
   selector: 'app-root',
-  imports: [PrimeiroComponenteComponent, DataBindingsComponent],
+  imports: [PrimeiroComponenteComponent, DataBindingsComponent, FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
