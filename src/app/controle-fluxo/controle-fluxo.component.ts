ab1b9d5bb08ddaca7c251f137bd6d614de06674c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-23 08:36:12 -0300

    Desenvolver diretivas

--- src/app/controle-fluxo/controle-fluxo.component.ts
+++ src/app/controle-fluxo/controle-fluxo.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-controle-fluxo',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './controle-fluxo.component.html',
   styleUrl: './controle-fluxo.component.css'
 })
