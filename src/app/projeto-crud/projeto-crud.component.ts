a693b03d9208c38c1d9c4954e5ea51791d320bdb
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-11 15:20:23 -0300

    Desenvolvimento de logica com angular para desenvolver o formulário

--- src/app/projeto-crud/projeto-crud.component.ts
+++ src/app/projeto-crud/projeto-crud.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-projeto-crud',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './projeto-crud.component.html',
   styleUrl: './projeto-crud.component.css'
 })
@@ -10,4 +11,11 @@ export class ProjetoCrudComponent {
 
     //Variável para trabalhar com avisibilidade dos botões
     btnCadastrar:boolean = true;
+
+    //Objeto do tipo FormGroup
+    pessoa = new FormGroup({
+        nome: new FormControl(''),
+        idade: new FormControl(''),
+        cidade: new FormControl('')
+    })
 }
