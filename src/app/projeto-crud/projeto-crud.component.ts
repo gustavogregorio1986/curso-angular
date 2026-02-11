4f67ee14a0ee96766c9e3e771af0300ecef51b6e
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-11 16:23:38 -0300

    Desenvolver o selecionar para aparecer cancelar, alterar e remover e deixar desabilitado o cadastro

--- src/app/projeto-crud/projeto-crud.component.ts
+++ src/app/projeto-crud/projeto-crud.component.ts
@@ -23,9 +23,25 @@ export class ProjetoCrudComponent {
     //Vetor para armazenar pessoas
     vetor:Pessoa[] = [];
 
+     //Inidice da pessoa selecionada
+     indicePessoaSelecionada:number = -1;
+
+
      //Método para efetuar o cadastro
      cadastrar():void{
         this.vetor.push(this.pessoa.value as Pessoa);
         this.pessoa.reset();
+     }
+
+     //Método para selecionar uma pessoa especifica
+     selecionar(indice:number):void{
+         this.indicePessoaSelecionada = indice;
+
+         this.pessoa.get('nome')?.setValue(this.vetor[indice].nome || '');
+         this.pessoa.get('idade')?.setValue(this.vetor[indice].idade?.toString() || '');
+         this.pessoa.get('cidade')?.setValue(this.vetor[indice].cidade || '');
+
+         this.btnCadastrar = false;
+     
      }
 }
